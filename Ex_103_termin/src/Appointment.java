503e141a8cf905f559ecb175791056b5309e19f9
neukoc16 <neukoc16@new.edvhtl.at>
2018-10-05 15:14:52 +0200

    created methods in Appointment

--- Ex_103_termin/src/Appointment.java
+++ Ex_103_termin/src/Appointment.java
@@ -1,7 +1,39 @@
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 /**
  * @author Konstantin
  */
 public class Appointment {
+
+    private LocalDateTime date;
+    private String text;
+
+    public Appointment(LocalDateTime date, String text) {
+        this.date = date;
+        this.text = text;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s ---> %s", date.format(DateTimeFormatter.ofPattern("dd.MM.yyy - HH.mm")), text);
+    }
 
 }
